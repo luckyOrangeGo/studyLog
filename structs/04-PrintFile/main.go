860dee7f95ab7e807d40fe7b5e734f50c1eb15ad
AfireCheng_OnTP <luckyOrangeGo@.github.com>
2018-04-23 00:20:28 +0800

    Readme

--- structs/04-PrintFile/main.go
+++ structs/04-PrintFile/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -8,10 +9,43 @@ import (
 
 func main() {
 	f, err := os.Open(os.Args[1])
+	defer f.Close()
+
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
 	io.Copy(os.Stdout, f)
+
+	newMain()
+}
+
+func print() {
+	fmt.Println("output")
+}
+
+func newMain() {
+	old := os.Stdout // keep backup of the real stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	print()
+
+	outC := make(chan string)
+	// copy the output in a separate goroutine so printing can't block indefinitely
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outC <- buf.String()
+	}()
+
+	// back to normal state
+	w.Close()
+	os.Stdout = old // restoring the real stdout
+	out := <-outC
+
+	// reading our temp stdout
+	fmt.Println("previous output:", old)
+	fmt.Print(out)
 }
