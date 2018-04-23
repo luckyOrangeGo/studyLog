9e197a1fbd3f0ae7e6e2dbd94decbff07f7489f9
AfireCheng_OnTP <luckyOrangeGo@.github.com>
2018-04-23 21:42:26 +0800

    channel 域名访问测试计数完成

--- channels/README.md
+++ channels/README.md
@@ -2,10 +2,10 @@
 
 将原本按顺序进行的程序，并行进行
 
-## channel01
-
 for 循环使URL按顺序被访问
 
-## channle02
-
 go routine -- Engine that executes code
+
+使用map作为链接访问次数计数器
+
+使用匿名函数作为延迟器，代替在main函数中延迟
--- channels/main.go
+++ channels/main.go
@@ -1,8 +1,11 @@
+// 访问域名并打印可访问性，和访问次数
+
 package main
 
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -14,18 +17,32 @@ func main() {
 		"http://golang.org",
 	}
 
-	c := make(chan string) //在channel 中
+	// channel
+	c := make(chan string)
 
+	// 计数器
+	m := make(map[string]int)
+
+	// 独立进程分配
 	for _, link := range links {
-		go checkLink(link, c) // 在函数前面加go 使其变成独立线程
+		m[link] = 0
+		go checkLink(link, c, m) // 在函数前面加go 使其变成独立线程
+
 	}
 
-	for {
-		go checkLink(<-c, c)
+	//延迟1秒后重复访问
+	for l := range c {
+		go func(link string) { //function literal 需要传入的参数
+			time.Sleep(1 * time.Second)
+			checkLink(link, c, m)
+		}(l) //实际传入的参数
+
 	}
+
 }
 
-func checkLink(link string, c chan string) {
+// 检查链接可访问性，并记录访问次数
+func checkLink(link string, c chan string, m map[string]int) {
 	_, err := http.Get(link) //单线程在这里卡住了
 	if err != nil {
 		fmt.Println(link, "might be down!")
@@ -33,6 +50,9 @@ func checkLink(link string, c chan string) {
 		return
 	}
 
-	fmt.Println(link, "is up!")
+	//计数器++
+	m[link]++
+
+	fmt.Println(link, "is up!", m[link])
 	c <- link
 }
