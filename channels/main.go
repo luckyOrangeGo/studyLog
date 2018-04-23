32bd9d644a637380170bd697675b97b64587bc6f
AfireCheng_OnTP <luckyOrangeGo@.github.com>
2018-04-23 20:03:56 +0800

    所有进程完成后停止

--- channels/main.go
+++ channels/main.go
@@ -13,17 +13,26 @@ func main() {
 		"http://qq.com",
 		"http://golang.org",
 	}
+
+	c := make(chan string) //在channel 中
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c) // 在函数前面加go 使其变成独立线程
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link) //单线程在这里卡住了
 	if err != nil {
-		fmt.Println(link, "might be donw!")
+		fmt.Println(link, "might be down!")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- link
 }
